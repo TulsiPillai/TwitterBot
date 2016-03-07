62289efbb9db26abe5beb28572fa3ae7ab300dee
Thulasi <tulsi.sv1989@gmail.com>
2016-03-07 15:05:34 -0500

    Create follow.py

---
+++ follow.py
@@ -0,0 +1,15 @@
+import tweepy
+
+#enter the corresponding information from your Twitter application:
+CONSUMER_KEY = ""
+CONSUMER_SECRET = "" 
+ACCESS_KEY = ""
+ACCESS_SECRET = ""
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+for follower in tweepy.Cursor(api.followers).items():
+follower.follow()
+print follower.screen_name
